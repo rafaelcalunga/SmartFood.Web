11bd99d4b4ece867ac715f8b7981ce2d4c020091
Rafael Calunga <rafaelcalunga@gmail.com>
2022-03-04 16:45:54 -0500

    Add React-hook-form to control form states

--- src/components/recipes/RecipeForm.tsx
+++ src/components/recipes/RecipeForm.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap';
+import { Controller, SubmitHandler, useForm, useWatch } from 'react-hook-form';
 import { ICategory } from '../../models/ICategory';
 import api from '../../services/api';
 
@@ -8,8 +9,19 @@ interface IProps {
   onClose: () => void;
 }
 
+interface IFormValues {
+  name: string;
+  preparationTime: number;
+  servings: number;
+  ingredients: string;
+  description: string;
+  category: ICategory;
+}
+
 const RecipeForm: React.FC<IProps> = ({ show, onClose }: IProps) => {
   const [categories, setCategories] = useState<Array<ICategory>>([]);
+  const { control, handleSubmit, setValue } = useForm<IFormValues>();
+  const categoryId = useWatch({ control, name: 'category.id' });
 
   useEffect(() => {
     getCategories();
@@ -20,54 +32,102 @@ const RecipeForm: React.FC<IProps> = ({ show, onClose }: IProps) => {
       .get('/categories')
       .then((response) => {
         setCategories(response.data);
+        if (response.data.length > 0) {
+          setValue('category.id', response.data[0].id);
+        }
       })
       .catch((error) => console.error(error));
   };
 
+  const onSubmit: SubmitHandler<IFormValues> = (data: IFormValues) => {
+    if (data.category.id) {
+      const categoryName = categories.find((category) => category.id === data.category.id)?.name;
+      if (categoryName) {
+        data.category.name = categoryName;
+      }
+    }
+
+    console.log(data);
+    //TODO: add post logic here...
+  };
+
   return (
     <Modal show={show}>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Modal.Header>
           <Modal.Title>Add Recipe</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control type="text" />
+            <Controller
+              name="name"
+              control={control}
+              render={({ field }) => <Form.Control type="text" {...field} />}
+            />
           </Form.Group>
           <Row className="mb-3">
             <Form.Group as={Col}>
               <Form.Label>Time (m)</Form.Label>
-              <Form.Control type="number" />
+              <Controller
+                name="preparationTime"
+                control={control}
+                render={({ field }) => <Form.Control type="number" {...field} />}
+              />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Servings</Form.Label>
-              <Form.Control type="number" />
+              <Controller
+                name="servings"
+                control={control}
+                render={({ field }) => <Form.Control type="number" {...field} />}
+              />
             </Form.Group>
           </Row>
-          <Form.Group className="mb-3">
-            <Form.Label>Category</Form.Label>
-            <Form.Select>
-              <option value="">Choose your category</option>
-              {categories.map((category: ICategory) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
-                </option>
-              ))}
-              <option>Add new category...</option>
-            </Form.Select>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>New Category</Form.Label>
-            <Form.Control type="text" />
-          </Form.Group>
+          {categories.length > 0 && (
+            <Form.Group className="mb-3">
+              <Form.Label>Category</Form.Label>
+              <Controller
+                name="category.id"
+                control={control}
+                render={({ field }) => (
+                  <Form.Select {...field}>
+                    {categories.map((category: ICategory) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
+                    <option value="">Add new category...</option>
+                  </Form.Select>
+                )}
+              />
+            </Form.Group>
+          )}
+          {(categoryId === '' || categories.length === 0) && (
+            <Form.Group className="mb-3">
+              <Form.Label>New Category</Form.Label>
+              <Controller
+                name="category.name"
+                control={control}
+                render={({ field }) => <Form.Control type="text" {...field} />}
+              />
+            </Form.Group>
+          )}
           <Form.Group className="mb-3">
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" />
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => <Form.Control type="text" {...field} />}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Ingredients</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Controller
+              name="ingredients"
+              control={control}
+              render={({ field }) => <Form.Control as="textarea" rows={3} {...field} />}
+            />
           </Form.Group>
         </Modal.Body>
         <Modal.Footer>
